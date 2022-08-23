65636fd68d5cc73d178946563f1861c0831231b4
Filipe Barbosa Ferreira <filipefbf@ibm.com>
2022-08-23 15:43:30 -0300

    User entitiy e resource

---
+++ src/main/java/com/filipeltda/course/entities/User.java
@@ -0,0 +1,36 @@
+package com.filipeltda.course.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
