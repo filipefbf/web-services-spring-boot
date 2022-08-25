064c09a0e99cf8d9d1ff770aaace21b4b7218710
Filipe Barbosa Ferreira <filipefbf@ibm.com>
2022-08-25 09:55:12 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/filipeltda/course/entities/User.java
+++ src/main/java/com/filipeltda/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.filipeltda.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +29,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
