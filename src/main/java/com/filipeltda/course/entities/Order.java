83dc6576bbbb5c8f539fddd05b8f0466a5ebcdf7
Filipe Barbosa Ferreira <filipefbf@ibm.com>
2022-08-25 12:38:12 -0300

    OrderStatus enum

--- src/main/java/com/filipeltda/course/entities/Order.java
+++ src/main/java/com/filipeltda/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.filipeltda.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.filipeltda.course.entities.enums.OrderStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +27,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -33,6 +36,23 @@ public class Order implements Serializable {
     public Order(Long id, Instant moment) {
         this.id = id;
         this.moment = moment;
+    }
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
